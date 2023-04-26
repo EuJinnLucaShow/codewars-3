28ae6fb31ac4f5b6c7bbfd06d8f3f3b3c3880945
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2023-04-26 23:06:52 +0300

    obj[language]

--- js/task063.js
+++ js/task063.js
@@ -1,1 +1,25 @@
+function greet(language) {
+  const obj = {
+    english: 'Welcome',
+    czech: 'Vitejte',
+    danish: 'Velkomst',
+    dutch: 'Welkom',
+    estonian: 'Tere tulemast',
+    finnish: 'Tervetuloa',
+    flemish: 'Welgekomen',
+    french: 'Bienvenue',
+    german: 'Willkommen',
+    irish: 'Failte',
+    italian: 'Benvenuto',
+    latvian: 'Gaidits',
+    lithuanian: 'Laukiamas',
+    polish: 'Witamy',
+    spanish: 'Bienvenido',
+    swedish: 'Valkommen',
+    welsh: 'Croeso',
+  }
+  return `${obj[language]}. Try again.`
+}
 
+console.log(greet('dutch'))
+console.log(greet('english'))
