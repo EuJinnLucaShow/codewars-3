b6321e68fb6ff1dd202f1514f5d549b3477baaa6
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2023-04-27 20:53:29 +0300

    array.push(x * i)
    for (let i = x; z.length < n; i++) if (i % x === 0) z.push(i)

---
+++ js/task065.js
@@ -0,0 +1,28 @@
+/*Create a function with two arguments that will return an array of the first n multiples of x.
+
+Assume both the given number and the number of times to count will be positive numbers greater than 0.
+
+Return the results as an array or list ( depending on language ).
+
+Examples
+countBy(1,10) === [1,2,3,4,5,6,7,8,9,10]
+countBy(2,5) === [2,4,6,8,10]*/
+
+// function countBy(x, n) {
+//   let z = []
+//   for (let i = x; z.length < n; i++) if (i % x === 0) z.push(i)
+//   return z
+// }
+
+function countBy(x, n) {
+  const array = []
+
+  for (let i = 1; i <= n; i++) {
+    array.push(x * i)
+  }
+
+  return array
+}
+
+console.log(countBy(1, 10)) //  [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+console.log(countBy(2, 5)) //  [2, 4, 6, 8, 10]
