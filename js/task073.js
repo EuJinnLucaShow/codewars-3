eaaa32db593ee3e5d2b3ffcf9f4417d274f6baaa
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2023-04-30 12:28:19 +0300

    Update task073.js

--- js/task073.js
+++ js/task073.js
@@ -1,5 +1,20 @@
-function reverseList(list) {
-  return list.reverse()
+function count(string) {
+  let obj = {}
+
+  for (let i = 0; i < string.length; i++) {
+    let char = string.charAt(i)
+    if (obj[char]) {
+      obj[char]++
+    } else {
+      obj[char] = 1
+    }
+  }
+
+  return obj
 }
 
-console.log(reverseList([1, 2, 3, 4]))
+console.log(count('abas')) //
+
+/*The main idea is to count all the occurring characters in a string. If you have a string like aba, then the result should be {'a': 2, 'b': 1}.
+
+What if the string is empty? Then the result should be empty object literal, {}.*/
