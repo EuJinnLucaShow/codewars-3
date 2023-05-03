688e23f01e1084c93ab786d8a963daf7d638969f
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2023-05-03 23:44:50 +0300

    Create task080.js

---
+++ js/task080.js
@@ -0,0 +1,37 @@
+/*The Fibonacci numbers are the numbers in the following integer sequence (Fn):
+
+0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, ...
+
+such as
+
+F(n) = F(n-1) + F(n-2) with F(0) = 0 and F(1) = 1.
+
+Given a number, say prod (for product), we search two Fibonacci numbers F(n) and F(n+1) verifying
+
+F(n) * F(n+1) = prod.
+
+Your function productFib takes an integer (prod) and returns an array:
+
+[F(n), F(n+1), true] or {F(n), F(n+1), 1} or (F(n), F(n+1), True)
+depending on the language if F(n) * F(n+1) = prod.
+
+If you don't find two consecutive F(n) verifying F(n) * F(n+1) = prodyou will return
+
+[F(n), F(n+1), false] or {F(n), F(n+1), 0} or (F(n), F(n+1), False)
+F(n) being the smallest one such as F(n) * F(n+1) > prod.*/
+
+function productFib(prod) {
+  // Initialize the first two Fibonacci numbers
+  let a = 0,
+    b = 1
+
+  // Iterate over the Fibonacci sequence
+  while (a * b < prod) {
+    let temp = b
+    b = a + b
+    a = temp
+  }
+
+  // Return the result as an array
+  return [a, b, a * b === prod]
+}
